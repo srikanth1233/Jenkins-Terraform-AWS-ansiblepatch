67b14adb1d09ce386ca445067091152416f97297
srikanth1233 <57040285+srikanth1233@users.noreply.github.com>
2023-10-21 17:50:57 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,6 @@ value = aws_instance.jenkins.public_ip
 output "server_id" {
 value = aws_instance.jenkins.id
 }
+output hello_ec2_private_ip {
+  value = "${join(",",aws_instance.jenkins.*.private_ip)}"
+}
