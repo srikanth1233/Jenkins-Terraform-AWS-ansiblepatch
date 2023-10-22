cb0c8d04b3b8a6cd29dc106c9ef8e62b404edcce
srikanth1233 <57040285+srikanth1233@users.noreply.github.com>
2023-10-22 15:25:58 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -26,6 +26,20 @@ resource "local_file" "tf-key" {
   content  = tls_private_key.rsa.private_key_pem
   filename = "tf-key-pair-${random_id.server.hex}"
 }
+resource "local_file" "hosts_cfg" {
+  content = templatefile("inventory.tmpl",
+    {
+      ubuntu_hosts = aws_instance.web.*.public_ip
+    }
+  )
+  filename = "/home/devops/hosts"
+}
+
+
+
+output "server_private_ip" {
+  value = aws_instance.web.private_ip
+}
 /*
 resource "aws_instance" "Ansible" {
   ami           = "ami-051f7e7f6c2f40dc1"
